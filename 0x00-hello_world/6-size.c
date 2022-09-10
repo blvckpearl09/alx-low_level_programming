2ab961f4e732ab6179542c6f789f45b6c1733259
Mustie <rolinwitbunmi@gmail.com>
2022-09-09 23:11:16 -0700

    Code Troubleshoot

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  *  main - Entry Point
  *
@@ -7,6 +8,7 @@
  *
  *  Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
@@ -14,5 +16,6 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n". (int) sizeof(float));
+	
 	return (0);
 }
