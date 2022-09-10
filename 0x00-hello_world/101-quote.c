87f485338f004f0704b8c7304f2f6e9651164f5c
Mustie <rolinwitbunmi@gmail.com>
2022-09-09 23:21:34 -0700

    Damn C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 #include <unistd.h>
 
@@ -15,4 +14,3 @@ int main(void)
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-
