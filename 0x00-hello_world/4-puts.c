fba1785d7c382331ab2da9478bfa60af92014105
Mustie <rolinwitbunmi@gmail.com>
2022-09-09 20:38:34 -0700

    SECOND PRINT

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
  /**
  * main - Entry point
  *
- * Program prints "Programming is like building a multilingual puzzle"
+ * Description: prints "Programming is like building a multilingual puzzle"
  *
  * Return: Always 0 (Success)
  */
