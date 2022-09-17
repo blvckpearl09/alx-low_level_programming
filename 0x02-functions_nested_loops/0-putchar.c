1f8725aa1c3bcd4e079d4b82556bb4e940b97cfc
Mustie <rolinwitbunmi@gmail.com>
2022-09-16 23:12:37 -0700

    PC

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char pChar[8] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(pChar[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
