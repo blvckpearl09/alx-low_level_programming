6b929170ce4ae731ba9ee7f04b4f50f2da94da1a
Mustie <rolinwitbunmi@gmail.com>
2022-09-17 01:01:39 -0700

    ABS

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @r: r is an integer
+ *
+ * Return: r (Bingo)
+ */
+
+int _abs(int)
+{
+	if (r > 0)
+	{
+		return (r);
+	}
+	else if (r < 0)
+	{
+		return (-r);
+	}
+	else
+	{
+		return (r);
+	}
+}
