3bc699bc4b0dc32316f032772906e5919b2cc321
Mustie <rolinwitbunmi@gmail.com>
2022-09-17 00:05:36 -0700

    ALCx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10
+ *
+ * Description - Prints alphabets in lowercase 10times
+ *
+ * Return: 0 (Bingo)
+ */
+
+void print_alphabet_x10(void)
+{
+	char alphabet = 'a';
+	int round = 0;
+
+	while (round < 10)
+	{
+		alphabet = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(alphabet);
+			alphabet++;
+		}
+		_putchar('\n');
+
+		round++;
+	}
+}
