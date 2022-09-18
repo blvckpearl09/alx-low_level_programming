ec6d8ad4fe11091d9a87f6afb196d17508d9472a
Mustie <rolinwitbunmi@gmail.com>
2022-09-18 09:38:55 -0700

    TimesTable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,10 +17,10 @@ void times_table(void)
 		{
 			product = row * column;
 			tens = product / 10;
-			units = product % 10;;
+			units = product % 10;
 
 			if (column == 0)
-			{	
+			{
 				_putchar('0');
 			}
 			else if (product < 10)
