086b9101545d62c5d0073d9a942423a11a5d7ced
Mustie <rolinwitbunmi@gmail.com>
2022-09-18 12:09:56 -0700

    FBC-98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,30 +5,42 @@
  *
  * Return: 0 (Bingo)
  */
-
 int main(void)
 {
-	unsigned long int sum, first, second;
-	int count;
+	int inc;
+	unsigned long n1 = 0, n2 = 1, n3;
+	unsigned long n1_h1, n1_h2, n2_h1, n2_h2;
+	unsigned long h1, h2;
 
-	sum = 0;
-	first = 0;
-	second = 1;
-	count = 0;
-
-	while (count < 98)
+	for (inc = 0; inc < 92; inc++)
 	{
-		sum = first + second;
-		printf("%lu", sum);
-		first = second;
-		second = sum;
-
-		if (count < 97)
-			printf(", ");
-		count++;
+		n3 = n1 + n2;
+		printf("%lu, ", n3);
+		n1 = n2;
+		n2 = n3;
 	}
+	n1_h1 = n1 / 10000000000;
+	n2_h1 = n2 / 10000000000;
+	n1_h2 = n1 % 10000000000;
+	n2_h2 = n2 % 10000000000;
+	for (inc = 93; inc < 99; inc++)
+	{
+		h1 = n1_h1 + n2_h1;
+		h2 = n1_h2 + n2_h2;
+		if ((n1_h2 + n2_h2) > 9999999999)
+		{
+			h1 += 1;
+			h2 %= 10000000000;
+		}
+		printf("%lu%lu", h1, h2);
+		if (inc != 98)
+			printf(", ");
 
+		n1_h1 = n2_h1;
+		n1_h2 = n2_h2;
+		n2_h1 = h1;
+		n2_h2 = h2;
+	}
 	printf("\n");
-
 	return (0);
 }
