a506a317e1566ffdf4dd7e5a33b87acccd7f666c
Mustie <rolinwitbunmi@gmail.com>
2022-09-18 18:07:08 -0700

    LSC

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
- * main - est that the function positive_or_negative()
- *  gives the correct output when given a case of 0
- *
- *  Return: 0 (Bingo)
+ * main - test that the function positive_or_negative
+ *  Return: 0
  */
 
 int main(void)
