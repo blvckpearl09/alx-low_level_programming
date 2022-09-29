9e5dac2d08376f9304d3178c14ae891ec784a5c2
blvckpearl09 <rolinwitbunmi@gmail.com>
2022-09-29 05:49:07 +0100

    Inception

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,23 +9,22 @@
 
 int evaluate_num(int num, int iterator)
 {
-
-if (iterator == num - 1)
+  if (iterator == num - 1)
 {
-return (1);
+    return (1);
 }
 
-else if (num % iterator == 0)
+  else if (num % iterator == 0)
 {
-return (0);
+    return (0);
 }
 
-if (num % iterator != 0)
+  if (num % iterator != 0)
 {
-return (evaluate_num(num, iterator + 1));
+    return (evaluate_num(num, iterator + 1));
 }
 
-return (0);
+  return (0);
 
 }
 
@@ -37,21 +36,18 @@ return (0);
 
 int is_prime_number(int num)
 {
-
-int iterator;
-
-iterator = 2;
+  
+  int iterator;
+  iterator = 2;
 
 /* only greater than 2*/
-iif (num < 2)
+  if (num < 2)
 {
-return (0);
+    return (0);
 }
-
-if (num == 2)
+  if (num == 2)
 {
-return (1);
+    return (1);
 }
-
-return (evaluate_num(num, iterator));
+  return (evaluate_num(num, iterator));
 }
