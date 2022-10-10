c685b5a2c28ad18afa64efa7854f9f8debed25bd
blvckpearl09 <rolinwitbunmi@gmail.com>
2022-10-10 17:56:27 +0100

    REFIX

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase char
+ *
+ * @c: c is an ascii character
+ *
+ * Return: 1 (If c = lowercase)
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
